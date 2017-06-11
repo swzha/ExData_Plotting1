ae5deaf77e34dc5f55590a612af13de844c860a9
swzha <swzha@hotmail.com>
2017-06-11 00:24:42 -0400

    Add files via upload

---
+++ plot1.R
@@ -0,0 +1,17 @@
+## read the data and convert "Date" column to a Date class
+df <- read.table("household_power_consumption.txt", sep=";", stringsAsFactors=FALSE, head=TRUE)
+library(lubridate)
+df$Date <-as.Date(df$Date, "%d%m%Y")
+library(dplyr)
+
+## subset the data frame to only include the two days
+df2 <- filter(df, between(Date, as.Date("2007-02-01"), as.Date("2007-02-02")))
+
+## plot1 using default Windows graphics device
+hist(as.numeric(df2$Global_active_power), xlab ="Global Active Power (kilowatts)", main ="Global Active POwer", col = "red")
+
+## plot1.png
+png(file="plot1.png")
+hist(as.numeric(df2$Global_active_power), xlab ="Global Active Power (kilowatts)", main ="Global Active Power", col = "red")
+dev.off()
+
